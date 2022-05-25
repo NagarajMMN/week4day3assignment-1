afb332a026d6ff73485e921d401cc39e6a1967ee
NagarajMMN <105267768+NagarajMMN@users.noreply.github.com>
2022-05-25 12:27:07 +0530

    Add files via upload

---
+++ system/Desktop.java
@@ -0,0 +1,19 @@
+package org.system;
+
+public class Desktop extends Computer {
+	
+	 public void desktopSize( ) {
+		 System.out.println("the desktop size is :");
+	 }
+	 
+	 public static void main(String[] args) {
+		Desktop obj=new Desktop();
+		obj.computerModel();
+		obj.desktopSize();
+		
+		
+	}
+
+	
+
+}
